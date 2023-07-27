03ce2e08f1b8b7d7379e170595000f0329cb4788
Jenniffer Melchiade <jdenisemelch@gmail.com>
2023-07-27 17:32:15 -0400

    lab 2 exercise to test syntax errors and alternative outputs

---
+++ Lab2.java
@@ -0,0 +1,11 @@
+class Mystry {
+    public static void main(String[] arg) {
+        int i = 10;
+        int j = 12;
+        if (i < j && i <= 3) {
+            System.out.println("i is " + i);
+        } else {
+            System.out.println("i is not less than or equal to three");
+        }
+    }
+}
